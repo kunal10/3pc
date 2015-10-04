e955fc43b059ce1ba59be89afcea84d015bdb230
Kunal Lad <kunallad423@gmail.com>
2015-10-03 22:53:19 -0500

    Adding description for Message class.

--- src/dc/Message.java
+++ src/dc/Message.java
@@ -1,7 +1,15 @@
 package dc;
 
 /**
- *
+ * Abstract class which captures basic functionalities of any message which is 
+ * exchanged between different processes. Messages are classified on the basis
+ * of process type of sender and receiver. Currently we have 2 kinds of 
+ * messages:
+ * 
+ * 1. ControllerMessage
+ * 2. ProcessMessage
+ * 
+ * All message classes are expected to extend this abstract class.
  */
 public abstract class Message {
 
